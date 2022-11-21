b248381d85d349f514e7c73dad64d6122ced1b6f
sanvalencia <s.valencia.d@hotmail.com>
2022-11-20 20:21:02 -0500

    plantilla

---
+++ js/ace.js
@@ -0,0 +1,14 @@
+(function($) {
+  'use strict';
+  var editor = ace.edit("aceExample");
+  editor.setTheme("ace/theme/chaos");
+  editor.getSession().setMode("ace/mode/javascript");
+  document.getElementById('aceExample').style.fontSize = '1rem';
+})(jQuery);
+
+$(document).ready(function () {
+    $('#datatable').dataTable();
+
+    $("[data-toggle=tooltip]").tooltip();
+
+});
